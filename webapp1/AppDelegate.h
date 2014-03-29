2168369611bc9e2ea1d64fe338d0fb9937a9fb32
Pedro Cardoso <pcardoso@insomni.org>
2014-03-29 15:14:46 +0000

    Initial Commit

---
+++ webapp1/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  webapp1
+//
+//  Created by Pedro Cardoso on 29/03/14.
+//  Copyright (c) 2014 Clear Hybrid Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ webapp1/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  webapp1
+//
+//  Created by Pedro Cardoso on 29/03/14.
+//  Copyright (c) 2014 Clear Hybrid Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
