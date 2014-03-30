da5964084d93cc304ce63c475642b0f5f551e05b
Pedro Cardoso <pcardoso@insomni.org>
2014-03-30 17:12:12 +0100

    first commit

---
+++ webapp1/WebViewController.h
@@ -0,0 +1,13 @@
+//
+//  WebViewController.h
+//  webapp1
+//
+//  Created by Pedro Cardoso on 29/03/14.
+//  Copyright (c) 2014 Clear Hybrid Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WebViewController : UIViewController
+
+@end
---
+++ webapp1/WebViewController.m
@@ -0,0 +1,49 @@
+//
+//  WebViewController.m
+//  webapp1
+//
+//  Created by Pedro Cardoso on 29/03/14.
+//  Copyright (c) 2014 Clear Hybrid Apps. All rights reserved.
+//
+
+#import "WebViewController.h"
+
+@interface WebViewController ()
+
+@end
+
+@implementation WebViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
